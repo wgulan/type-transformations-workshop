49f468f4b3a99c8078c6eb5d6e2583a205ea97c0
w.gulan <wojciech.gulan@gmail.com>
2023-11-28 21:22:18 +0100

    03/14 exercise added

--- src/02-unions-and-indexing/13-get-array-value.problem.ts
+++ src/02-unions-and-indexing/13-get-array-value.problem.ts
@@ -3,7 +3,6 @@ import { Equal, Expect } from "../helpers/type-utils";
 const fruits = ["apple", "banana", "orange"] as const;
 
 type Fruits = typeof fruits
-
 type AppleOrBanana = Fruits[0 | 1];
 type Fruit = Fruits[number];
 
