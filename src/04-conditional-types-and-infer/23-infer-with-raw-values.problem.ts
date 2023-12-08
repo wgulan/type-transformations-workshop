165673f35ce469e796cbe6b45b42367df5e3988c
w.gulan <wojciech.gulan@gmail.com>
2023-12-08 19:28:42 +0100

    04/ exercises added

--- src/04-conditional-types-and-infer/23-infer-with-raw-values.problem.ts
+++ src/04-conditional-types-and-infer/23-infer-with-raw-values.problem.ts
@@ -1,17 +1,17 @@
-import { Equal, Expect } from "../helpers/type-utils";
+import { Equal, Expect } from '../helpers/type-utils';
 
-type GetDataValue<T> = unknown;
+type GetDataValue<T> = T extends { data: infer Data } ? Data : never;
 
 type tests = [
-  Expect<Equal<GetDataValue<{ data: "hello" }>, "hello">>,
-  Expect<Equal<GetDataValue<{ data: { name: "hello" } }>, { name: "hello" }>>,
+  Expect<Equal<GetDataValue<{ data: 'hello' }>, 'hello'>>,
+  Expect<Equal<GetDataValue<{ data: { name: 'hello' } }>, { name: 'hello' }>>,
   Expect<
     Equal<
-      GetDataValue<{ data: { name: "hello"; age: 20 } }>,
-      { name: "hello"; age: 20 }
+      GetDataValue<{ data: { name: 'hello'; age: 20 } }>,
+      { name: 'hello'; age: 20 }
     >
   >,
   // Expect that if you pass in string, it
   // should return never
-  Expect<Equal<GetDataValue<string>, never>>,
+  Expect<Equal<GetDataValue<string>, never>>
 ];
--- src/04-conditional-types-and-infer/27-infer-in-union-types.problem.ts
+++ src/04-conditional-types-and-infer/27-infer-in-union-types.problem.ts
@@ -1,19 +1,24 @@
-import { Equal, Expect } from "../helpers/type-utils";
+import { Equal, Expect } from '../helpers/type-utils';
 
 const parser1 = {
   parse: () => 1,
 };
 
-const parser2 = () => "123";
+const parser2 = () => '123';
 
 const parser3 = {
   extract: () => true,
 };
 
-type GetParserResult<T> = unknown;
+type GetParserResult<T> = T extends
+  | { parse: () => infer TResult }
+  | (() => infer TResult)
+  | { extract: () => infer TResult }
+  ? TResult
+  : never;
 
 type tests = [
   Expect<Equal<GetParserResult<typeof parser1>, number>>,
   Expect<Equal<GetParserResult<typeof parser2>, string>>,
-  Expect<Equal<GetParserResult<typeof parser3>, boolean>>,
+  Expect<Equal<GetParserResult<typeof parser3>, boolean>>
 ];
