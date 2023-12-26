e61c1c3cffbd72d9771b53967db06dda2e3d4d0b
w.gulan <wojciech.gulan@gmail.com>
2023-12-26 13:29:13 +0100

    05/33, 05/34, 05/35, 05/36 exercised added

--- src/05-key-remapping/33-discriminated-union-to-object.problem.ts
+++ src/05-key-remapping/33-discriminated-union-to-object.problem.ts
@@ -1,32 +1,36 @@
-import { Equal, Expect } from "../helpers/type-utils";
+import { Equal, Expect } from '../helpers/type-utils';
 
 type Route =
   | {
-      route: "/";
+      route: '/';
       search: {
         page: string;
         perPage: string;
       };
     }
-  | { route: "/about"; search: {} }
-  | { route: "/admin"; search: {} }
-  | { route: "/admin/users"; search: {} };
+  | { route: '/about'; search: {} }
+  | { route: '/admin'; search: {} }
+  | { route: '/admin/users'; search: {} };
 
-type RoutesObject = unknown;
+type Routes = Route['route'];
+
+type RoutesObject = {
+  [R in Routes]: Extract<Route, { route: R }>['search'];
+};
 
 type tests = [
   Expect<
     Equal<
       RoutesObject,
       {
-        "/": {
+        '/': {
           page: string;
           perPage: string;
         };
-        "/about": {};
-        "/admin": {};
-        "/admin/users": {};
+        '/about': {};
+        '/admin': {};
+        '/admin/users': {};
       }
     >
-  >,
+  >
 ];
